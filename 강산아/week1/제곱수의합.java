ba288608b96b39bc80c446e3f2ca9a8a5a564d68
sakang <rogersddt@naver.com>
2025-04-13 19:56:09 +0900

    week1 sando 제곱수의 합

---
+++ 강산아/week1/제곱수의합.java
@@ -0,0 +1,39 @@
+package week1;
+
+/**
+ * Author    : Kang San Ah
+ * Date      : 2025.04.13(Sun)
+ * Runtime   : 2 sec
+ * Memory    : 128 MB
+ * Algorithm : DP
+ */
+
+import java.io.*;
+import java.util.*;
+
+//https://www.acmicpc.net/problem/1699
+public class 제곱수의합 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N + 1];
+        Arrays.fill(dp, 100000);
+
+        for (int i = 1; i <= N; i++) {
+            int k = (int) Math.sqrt(i);
+
+            if (k * k == i) {
+                dp[i] = 1;
+            } else {
+                for (int j = 1; j <= k; j++) {
+                    dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+                }
+            }
+        }
+
+        System.out.println(dp[N]); // 출력
+    }
+}
