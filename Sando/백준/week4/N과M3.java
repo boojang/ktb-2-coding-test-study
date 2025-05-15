4a65e28d5cdf198611cec191d7373ae46929e2a4
sakang <rogersddt@naver.com>
2025-05-15 11:00:25 +0900

    Feat: 4주차 sando N과M3

---
+++ Sando/백준/week4/N과M3.java
@@ -0,0 +1,48 @@
+/**
+ * Author    : Kang San Ah
+ * Date      : 2025.05.15(Thu)
+ * Runtime   : 1 sec
+ * Memory    : 512 MB
+ * Algorithm : DFS
+ */
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static int [] output;
+    static int N, M;
+    static StringBuilder sb;
+
+    static void dfs(int depth){
+        if(depth == M){
+            for(int x : output){
+                sb.append(x).append(' ')''
+            }
+            sb.append('\n');
+            return;
+        }
+        else{
+            for(int i = 1; i <= N; i ++) {
+                output[depth] = i;
+                dfs(depth + 1);
+            }
+        }
+    }
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        output = new int[M];
+
+        dfs(0);
+
+        System.out.println(sb);
+    }
+}
