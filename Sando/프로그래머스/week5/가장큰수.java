138a3b4032fec9039950d6847ad4f7660865b411
sakang <rogersddt@naver.com>
2025-05-23 00:43:26 +0900

    Feat: 5주차 sando 가장 큰 수

---
+++ Sando/프로그래머스/week5/가장큰수.java
@@ -0,0 +1,25 @@
+package 프로그래머스.week5;
+
+import java.util.*;
+
+class 가장큰수 {
+    public String solution(int[] numbers) {
+
+        StringBuilder sb = new StringBuilder();
+        String[] arr = new String[numbers.length];
+
+        for(int i = 0 ; i< arr.length; i++){
+            arr[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(arr, (o1,o2) -> (o2+o1).compareTo(o1+o2));
+
+        if(arr[0].equals("0")) return "0";
+
+        for(String num : arr) {
+            sb.append(num);
+        }
+
+        return sb.toString();
+    }
+}
--- Sando/프로그래머스/week5/입국심사.java
+++ Sando/프로그래머스/week5/입국심사.java
@@ -1,26 +1,7 @@
 package 프로그래머스.week5;
 
-import java.util.Arrays;
+import java.util.*;
 
-class 입국심사 {
-    public String solution(int[] numbers) {
-        String[] stringArr = new String[numbers.length];
+class 입국심사{
 
-        for (int i  = 0 ; i < numbers.length; i ++){
-            stringArr[i] = String.valueOf(numbers[i]); // stringArr = ["6" , "10" , "2"]
-        }
-
-        Arrays.sort(stringArr, (o1, o2) -> (o2+o1).compareTo(o1+o2)); // ["6" , "2" , "10"]
-
-        if (stringArr[0].equals("0")) return "0"; // 조건
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0 ; i < stringArr.length; i++){
-            sb.append(stringArr[i]); // "6210"
-        }
-
-        return sb.toString(); // 문자열 변환
-
-    }
 }
