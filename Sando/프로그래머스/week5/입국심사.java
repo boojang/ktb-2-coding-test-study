5d7d6fbc2546f31f8c923a668ffb07b7a87d5a38
sakang <rogersddt@naver.com>
2025-05-27 13:34:41 +0900

    Feat: 5주차 sando 입국 심사

--- Sando/프로그래머스/week5/입국심사.java
+++ Sando/프로그래머스/week5/입국심사.java
@@ -3,5 +3,24 @@ package 프로그래머스.week5;
 import java.util.*;
 
 class 입국심사{
+    public  long solution(int n, int[] times) {
+        Arrays.sort(times); // 오름차 순 정렬. 시간이 적은 심사대 부터 심사 받기 위해
+        long min = 1 ; // 최소 1분이라 가정
 
+        long max = (long)times[times.length-1] * n ;
+        long sum ; long mid = 0; long answer = max;
+
+        while (min <= max){
+            sum = 0;
+            mid = (min + max) / 2;
+            for (int time : times) sum += mid / time;
+            if (sum >= n){
+                answer = mid;
+                max = mid - 1;
+            }else{
+                min = mid +1;
+            }
+        }
+        return answer;
+    }
 }
