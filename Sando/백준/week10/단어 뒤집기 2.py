b51b9075ad81d6f893ab1989912d52e6dd34e356
sakang <rogersddt@naver.com>
2025-07-14 18:24:17 +0900

    Feat: NBA 농구

--- Sando/백준/week10/단어 뒤집기 2.py
+++ Sando/백준/week10/단어 뒤집기 2.py
@@ -9,33 +9,29 @@
 import sys
 input = sys.stdin.readline
 
-def solve():
-    s = input().strip()
+def solution():
+    s = input()
     result = []
     i = 0
     
     while i < len(s):
         if s[i] == '<':
-            # 태그 처리: '<'부터 '>'까지 그대로 출력
             while i < len(s) and s[i] != '>':
                 result.append(s[i])
                 i += 1
-            if i < len(s):  # '>' 추가
+            if i < len(s):
                 result.append(s[i])
                 i += 1
         elif s[i] == ' ':
-            # 공백은 그대로 출력
             result.append(s[i])
             i += 1
         else:
-            # 단어 처리: 공백이나 '<'가 나올 때까지 수집 후 뒤집기
             word = []
             while i < len(s) and s[i] != ' ' and s[i] != '<':
                 word.append(s[i])
                 i += 1
-            # 단어를 뒤집어서 결과에 추가
             result.extend(reversed(word))
     
     return ''.join(result)
 
-print(solve())
+print(solution())
