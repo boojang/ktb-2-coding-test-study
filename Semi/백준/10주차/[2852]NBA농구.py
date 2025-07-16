6bf7447f9f2b65eeafba9fc3cf391d0ff1795469
HYUN <8135zxcvbn@gmail.com>
2025-07-16 13:46:14 +0900

    Refactor: 10주차 semi 코드 수정

--- Semi/백준/10주차/[2852]NBA농구.py
+++ Semi/백준/10주차/[2852]NBA농구.py
@@ -29,42 +29,43 @@ n = int(input())
 
 for i in range (n):
     a,b = input().split()
-    mm,ss = b.split(':') # 분,초를 나누다. + 초로 변환해서 저장,계산에 용이
+    mm,ss = b.split(':') # 분,초를 나누다.
+    goal_time = int(mm)*60 + int(ss) #초로 변환해서 저장,계산에 용이
 
     if a == '1': #1팀이 골을 넣음
-        # ★ 그 다음 다시 골을 넣어서 앞서기 시작하면 시작 시간을 갱신해야한다.
-        if(sc_1==sc_2): # 최초 이기고 있었을 때 시간 기록
-            time_1_last = int(mm)*60 + int(ss)
-
         sc_1 += 1
 
-        if(sc_1==sc_2): #다시 동점 -> 2팀이 이기고 있는 구간 끝
+        if sc_1 > sc_2 and sc_1-1 == sc_2: # 최초 이기고 있었을 때 시간 기록
+            time_1_last = goal_time
+
+        elif sc_1 == sc_2 : #다시 동점 -> 2팀이 이기고 있는 구간 끝
             #이기고 있던 시간 누적
-            time_2 += (int(mm)*60+int(ss)) - time_2_last
+            time_2 += goal_time - time_2_last
 
     else: # 2팀이 골을 넣음
-        if(sc_1==sc_2): # 최초 이기고 있었을 때
-            time_2_last = int(mm)*60 + int(ss)
         sc_2 += 1
 
-        if(sc_2==sc_1): #다시 동점
-            # 이기고 있던 시간 누적
-            time_1 += (int(mm) * 60 + int(ss)) - time_1_last
+        if sc_2 > sc_1 and sc_2-1 == sc_1: # 최초 이기고 있었을 때
+            time_2_last = goal_time
+
+        elif sc_2 == sc_1: #다시 동점
+            time_1 += goal_time - time_1_last
+
 
 
 # 최종결과
-if(sc_1>sc_2): #1팀 우승
+end_time = 48*60
+
+if sc_1 > sc_2: #1팀 우승
     # 48분 = 2880초
-    time = 2880 - time_1_last
-    time_1 += time
-else: #2팀 우승
-    time = 2880 - time_2_last
-    time_2 += time
+    time_1 += end_time - time_1_last
+elif sc_1 < sc_2: #2팀 우승
+    time_2 += end_time - time_2_last
 
 # 출력
 mm_1 = time_1 //60
 ss_1 = time_1 % 60
-print(f"{mm_1:02d}:{ss_1:02d}",end='\n')
+print(f"{mm_1:02d}:{ss_1:02d}")
 mm_2 = time_2 //60
 ss_2 = time_2 % 60
 print(f"{mm_2:02d}:{ss_2:02d}")
