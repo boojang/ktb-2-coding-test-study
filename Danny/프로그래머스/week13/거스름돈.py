236c7334db596651b37229a82a67c6b80b44f4ab
happy-yeachan <dus001228@naver.com>
2025-08-06 16:14:41 +0900

    Feat: 거스름돈

---
+++ Danny/프로그래머스/week13/거스름돈.py
@@ -0,0 +1,13 @@
+def solution(n, money):
+    dp = [0 for i in range(n+1)]
+    dp[0] = 1
+
+    for coin in money:
+        for price in range(coin, n+1):
+            if price >= coin:
+                dp[price] += dp[price-coin]
+
+    return dp[n]
+
+
+print(solution(5, [1, 2, 5])) #4
