5a22ffcec0748e89b96d7f1f6e60377854fa3e87
HYUN <8135zxcvbn@gmail.com>
2025-09-10 00:43:27 +0900

    Refactor: week18 semi 스타트와 링크 develop

--- Semi/백준/18주차/[14889]스타트와 링크.py
+++ Semi/백준/18주차/[14889]스타트와 링크.py
@@ -6,13 +6,6 @@ Memory    :
 Algorithm :
 '''
 
-# 총 n명
-# n 짝수
-# 1~n 사람수
-# Sij i번 사람과 j번 사람의 능력치
-
-# Sij 합의 차이가 최소화
-
 import sys
 from itertools import combinations
 
@@ -28,10 +21,12 @@ n = int(input())
 for i in range(n):
    s.append(list(map(int,input().split())))
 
-for s1 in combinations(range(n),n//2):
-    # print(f"s1 = { s1 }")
+# (+수정) 전체 집합을 두개의 동일한 그룹으로 나눌 때 기준이 되는 요소를 고정시킨다.
+# -> 경우의 수를 줄일 수 있음
+for s1 in combinations(range(1,n),n//2-1):
+    s1 = list(s1)
+    s1.append(0)
     s2 = set(range(n)) - set(s1)
-    # print(f"s2 = { s2 }")
 
     for a,b in combinations(s1,2):
         sum_1 += s[a][b]+s[b][a]
@@ -40,9 +35,6 @@ for s1 in combinations(range(n),n//2):
         sum_2 += s[c][d]+s[d][c]
 
     total_sum = min(total_sum,abs(sum_1-sum_2))
-    
-    # print(f"sum_1,sum_2 = { sum_1,sum_2 }")
-    # print(f"total_sum = {total_sum}")
     sum_1,sum_2=0,0
 
 print(total_sum)
