9a6e7fa265cd7205425d7006c461f669f1b30eee
HYUN <8135zxcvbn@gmail.com>
2025-11-19 17:15:43 +0900

    Refact: 99주차 semi 일곱난쟁이

--- Semi/백준/99주차/bronze/[2309]일곱난쟁이.py
+++ Semi/백준/99주차/bronze/[2309]일곱난쟁이.py
@@ -9,8 +9,12 @@ from itertools import combinations
 
 child = [int(input()) for _ in range(9)]
 
-for comb in combinations(child,7):
-    if sum(comb) == 100:
-        answer = sorted(comb)
-        print(*answer)
-        break
+total = sum(child)
+
+for i in range(len(child)):
+    for j in range(i,len(child)):
+        if total - child[i] - child[j] == 100:
+            answer = [child[k] for k in range(len(child)) if k!=i and k!=j]
+            answer.sort()
+            print(*answer)
+            exit()
